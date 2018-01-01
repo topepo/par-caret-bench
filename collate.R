7a151a86829ea5c975610a59af26cfd8be45ec90
Max Kuhn <max@imp.local>
2018-01-01 11:56:09 -0500

    small fixes

--- collate.R
+++ collate.R
@@ -3,9 +3,6 @@ library(ggplot2)
 theme_set(theme_bw())
 
 files <- list.files(pattern = "RData$", recursive = TRUE, full.names = TRUE)
-files2 <- list.files(pattern = "RData$", recursive = TRUE, full.names = TRUE,
-										 path = "/Volumes/max/github/par-caret-bench/macOS/2015 MacBookPro11,4 2.5 GHz Intel Core i7 4core")
-files <- c(files, files2)
 
 for (i in seq_along(files)) {
 	load(files[i])
@@ -46,7 +43,7 @@ ggplot(medians, aes(x = workers, y = speedup, col = computer)) +
 	geom_point(cex = 2) + 
 	geom_line() + 
 	geom_abline(col = "green") + 
-	theme(legend.position = "none")  + 
+	theme(legend.position = "top")  + 
 	ylab("Speed-Up") + 
 	xlab("# Workers") + 
 	facet_wrap(~method)
