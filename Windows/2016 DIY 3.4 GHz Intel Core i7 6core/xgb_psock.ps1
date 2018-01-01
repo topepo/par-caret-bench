6e574e8f7c9f31b0d4877ebca032313227065550
Max Kuhn <mxkuhn@gmail.com>
2018-01-01 17:00:15 -0500

    windows results and better scripts

---
+++ Windows/2016 DIY 3.4 GHz Intel Core i7 6core/xgb_psock.ps1
@@ -0,0 +1,45 @@
+﻿C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_07.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_06.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_13.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_08.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_03.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_09.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_04.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_02.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_12.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_01.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_15.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_05.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_10.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_14.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_psock_11.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_08.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_14.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_10.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_01.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_03.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_06.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_04.R
+Start-Sleep 120
+C:\R\R-3.4.3\bin\R.exe CMD BATCH --vanilla xgb_thred_13.R
